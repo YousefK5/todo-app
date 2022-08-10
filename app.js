644de14bde74074ba6c2e65d4fa516861ec3524c
YousefK5 <yousefk500@gmail.com>
2022-08-10 17:49:05 +0300

    changes

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-let name=prompt("Enter Your Name :");
+let names=prompt("Enter Your Name :");
 
-let gender= prompt(`Enter your Gender: "male or female" `);
+if(names !== null && names !=="") {
 
-if(gender !=="male" && gender!=="female") {
-    gender = prompt(`Re-Enter your Gender: please only "male" or "female"`);
+    let gender= prompt(`Enter your Gender: "male or female" `);
+
+    if(gender !=="male" && gender!=="female") {
+        gender = prompt(`Re-Enter your Gender: please only "male" or "female"`);
+    }
+
+    if(!(gender !=="male" && gender!=="female")) {
+
+        let age= prompt(`Enter Your Age :`);
+
+        if(+age<=0 || isNaN(+age)) {
+            alert(`Wrong Age`);
+        }
+
+        let skip=confirm("Are you want show Welcoming Message ?");
+        if(skip) {
+            alert(`Hello ${gender=="male" ? "Mr." : "Mrs."}${names} Welcome to My Website`);
+        }
+    } else {
+        alert("You Enter A Wrong Gender");
+    }
+} else {
+    alert("You Dont Enter Your Name");
 }
-
-let age= prompt(`Enter Your Age :`);
-
-if(+age<=0) {
-    alert(`Wrong Age`);
-}
-
-let skip=confirm("Are you want show Welcoming Message ?");
-
-if(skip) {
-    alert(`Hello ${gender=="male" ? "Mr." : "Mrs."}${name} Welcome to My Website`);
-}
-
-
